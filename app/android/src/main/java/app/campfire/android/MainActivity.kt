944a835b9636fadc4bc7fafc5e123a06eba3658d
Drew Heavner <veedubusc@gmail.com>
2025-02-05 20:15:13 -0500

    Fix Android Audio Playback (#94)
    Resolves #90

--- app/android/src/main/java/app/campfire/android/MainActivity.kt
+++ app/android/src/main/java/app/campfire/android/MainActivity.kt
@@ -1,5 +1,7 @@
 package app.campfire.android
 
+import android.app.Application
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,8 +13,12 @@ import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.lifecycleScope
 import app.campfire.android.di.ActivityComponent
+import app.campfire.audioplayer.impl.SessionActivityIntentProvider
+import app.campfire.core.di.AppScope
 import app.campfire.core.di.ComponentHolder
 import app.campfire.core.logging.bark
+import com.r0adkll.kimchi.annotations.ContributesBinding
+import me.tatarka.inject.annotations.Inject
 
 class MainActivity : ComponentActivity() {
 
@@ -66,4 +72,15 @@ private fun backDispatcherRootPop(): () -> Unit {
     LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
       ?: error("No OnBackPressedDispatcherOwner found, unable to handle root navigation pops.")
   return { onBackPressedDispatcher.onBackPressed() }
+}
+
+@ContributesBinding(AppScope::class)
+@Inject
+class MainActivityIntentProvider(
+  private val application: Application,
+) : SessionActivityIntentProvider {
+
+  override fun provide(): Intent {
+    return Intent(application, MainActivity::class.java)
+  }
 }
