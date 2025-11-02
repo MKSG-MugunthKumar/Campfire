05f05e5980b4dfd33de85f31f59e40ff178d7a10
Drew Heavner <veedubusc@gmail.com>
2025-11-02 15:48:42 -0500

    Refactored Home screen data pipeline (#409)

--- .idea/fileTemplates/Store Object.kt.child.1.kt
+++ .idea/fileTemplates/Store Object.kt.child.1.kt
@@ -1,17 +1,19 @@
-package app.campfire.home.store
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
 
+#end
+#parse("File Header.java")
 import app.campfire.data.mapping.asFetcherResult
 import app.campfire.network.AudioBookShelfApi
 import app.campfire.network.models.Shelf
 import org.mobilenativefoundation.store.store5.Fetcher
 
-class HomeFetcherFactory(
+class ${NAME}FetcherFactory(
   private val api: AudioBookShelfApi,
 ) {
 
-  fun create(): Fetcher<HomeStore.Key, List<Shelf>> {
+  fun create(): Fetcher<${NAME}Store.Key, Unit /* TODO: Network Model */> {
     return Fetcher.ofResult { key ->
-      api.getPersonalizedHome(key.libraryId).asFetcherResult()
+      TODO("Implement API")
     }
   }
 }
